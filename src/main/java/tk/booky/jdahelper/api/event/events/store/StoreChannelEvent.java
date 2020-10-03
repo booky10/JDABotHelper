3e3b2d44c29d8a8f6959e673a6751907d5b01000
booky10 <booky10@github.com>
2020-10-03 22:43:10 +0200

    <-- not so good in thinking

--- src/main/java/tk/booky/jdahelper/api/event/events/store/StoreChannelEvent.java
+++ src/main/java/tk/booky/jdahelper/api/event/events/store/StoreChannelEvent.java
@@ -1,7 +1,20 @@
 package tk.booky.jdahelper.api.event.events.store;
 // Created by booky10 in JDABotHelper (21:57 30.09.20)
 
-public class StoreChannelEvent {
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.StoreChannel;
+import tk.booky.jdahelper.api.event.api.Event;
 
+public class StoreChannelEvent extends Event {
 
+    protected final StoreChannel channel;
+
+    public StoreChannelEvent(JDA jda, Long response, StoreChannel channel) {
+        super(jda, response);
+        this.channel = channel;
+    }
+
+    public StoreChannel getChannel() {
+        return channel;
+    }
 }
